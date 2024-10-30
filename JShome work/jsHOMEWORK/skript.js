f4c184e705e61d7bf7682260baa192ff1e3081c8
Miras <miras.ismail.2003@mail.ru>
2024-10-31 00:29:57 +0300

    home.work

---
+++ JShome work/jsHOMEWORK/skript.js
@@ -0,0 +1,30 @@
+// бул типы string
+let name = "Miras"
+console.log("Аты: " + name+ "/ Типы :"+  typeof name);
+// бул типы number
+const yerth = 21
+
+console.log("Жасы: " + yerth+ "/Типы :"+ typeof yerth);
+
+// бул типы boolean
+let score = false
+if (true) {
+   let score = true
+   console.log("Студент пе?:" + score+ "/Типы: "+typeof score);
+   
+}
+
+// бул типы string
+var age = "Жасы"
+var age = "25"
+console.log("Жасы:"+  age+"/Типы:" + typeof age);
+
+
+
+
+
+
+
+
+
+
