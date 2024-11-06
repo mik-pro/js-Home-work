63bad340deffd8610d581569dd514040c7384fc9
Miras <miras.ismail.2003@mail.ru>
2024-11-06 09:03:08 +0300

    work

--- JShome work/jsHOMEWORK/skript.js
+++ JShome work/jsHOMEWORK/skript.js
@@ -1,13 +1,13 @@
-let num = Number (prompt ('бірінші  санды енгізіңіз:')) // 5 санын жаздым 
+// let num = Number (prompt ('бірінші  санды енгізіңіз:')) // 5 санын жаздым 
  
-let num1 = Number (prompt ('екінші  санды енгізіңіз:')) // 10 санын жаздым 
+// let num1 = Number (prompt ('екінші  санды енгізіңіз:')) // 10 санын жаздым 
    
-let num2 = Number (prompt ('үшінші  санды енгізіңіз:')) // 15 санын жаздым 
+// let num2 = Number (prompt ('үшінші  санды енгізіңіз:')) // 15 санын жаздым 
    
  
-var arifmetica = (num+num1+num2)/3 // жауабы 10 шығады 
+// var arifmetica = (num+num1+num2)/3 // жауабы 10 шығады 
  
-alert('ortasha mani: ' + arifmetica) // алерт арқылы жауабы шығады
+// alert('ortasha mani: ' + arifmetica) // алерт арқылы жауабы шығады
 
 
 
---
+++ homework2/jshomework2/skript.js
@@ -0,0 +1,34 @@
+// let age = 70;
+// let isStudent = false;
+// console.log("age<=65");
+let course = prompt("Курс нөмірін енгізіңіз (1, 2, 3 немесе 4):"); 
+let isAlmaty = prompt("Сіз Алматыдан келдіңіз бе? (я немесе жок)"); 
+ 
+if (course == "1" || course == "2") { 
+    if (isAlmaty == "no") { 
+        alert("Сізге жатақханадан орын беріледі."); 
+    } else { 
+        alert("Сізге жатақхана берілмейді."); 
+    } 
+} else { 
+    alert("Сізге жатақхана берілмейді."); 
+} 
+ 
+ 
+alert("Ойын шарты: әрбір дұрыс жауап үшін 1 ұпай беріледі."); 
+ 
+let surak1 = prompt("Қазақстанның тәуелсіздік алған жылы:"); 
+let surak2 = prompt("Қазір оқып жатқан қалаңыз:"); 
+ 
+let score = 0; 
+ 
+if (surak1 == "1991") { 
+    score += 1; 
+} 
+ 
+if (surak2 == "Almaty") { 
+    score += 1; 
+} 
+ 
+alert("Сіздің жинаған ұпайыңыз: " + score);
+
