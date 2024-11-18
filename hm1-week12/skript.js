cdb7da9f5b8f2a70faab91abd8bf0be8e5f6bf0f
Miras <miras.ismail.2003@mail.ru>
2024-11-18 17:27:45 +0300

    function

--- hm1-week12/skript.js
+++ hm1-week12/skript.js
@@ -1,16 +1,18 @@
-let soz =prompt("бырыншы соз енгыз")
-let soz2=prompt("екыншы соз")
+let soz =prompt("бырыншы соз енгыз").toLowerCase()
+let soz2=prompt("екыншы соз").toLowerCase()
 
  
-if (soz<=soz2) {
+if (soz==soz2) {
         function name(compareWords) {
-               soz(soz,soz2)
+               soz(soz)
+               soz2(soz2)
         }
-       alert(`бырдей создер`)   
+       alert("бырдей создер")   
        
 }
  
 else{
-        alert(`создер бырдей емес`)
+        alert("создер бырдей емес")
       
 }
+
