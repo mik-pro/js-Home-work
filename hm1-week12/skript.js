fdf6fd99ecd44e3d345c1ad31bc077b36a23453c
Miras <miras.ismail.2003@mail.ru>
2024-11-18 16:55:52 +0300

    function

---
+++ hm1-week12/skript.js
@@ -0,0 +1,16 @@
+let soz =prompt("бырыншы соз енгыз")
+let soz2=prompt("екыншы соз")
+
+ 
+if (soz<=soz2) {
+        function name(compareWords) {
+               soz(soz,soz2)
+        }
+       alert(`бырдей создер`)   
+       
+}
+ 
+else{
+        alert(`создер бырдей емес`)
+      
+}
