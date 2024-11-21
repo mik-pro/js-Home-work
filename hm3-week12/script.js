282893ddd137707c53dc49a6f250e04f12216fda
Miras <miras.ismail.2003@mail.ru>
2024-11-21 06:30:28 +0300

    valuta

--- hm3-week12/script.js
+++ hm3-week12/script.js
@@ -1,44 +1,37 @@
-function convertToEuro() {
-    let totalAmountInEuro = 0;
+function convert() {
+    let euro = 0;
+
     
+
     while (true) {
+       
+        let summa = prompt("Соманы енгізіңіз (немесе 'стоп' жазыңыз):");
+        if (summa.toLowerCase() === "стоп")
+             break;
+
         
-        let amount = prompt("Ақша сомасын енгізіңіз (немесе 'стоп' деп жазып тоқтатыңыз):");
-        if (amount.toLowerCase() === 'стоп') {
-            break; 
-        }
-        
-        let currency = prompt("Валютаны енгізіңіз (USD, KZT, EUR):").toUpperCase();
-        
-        
-        amount = parseFloat(amount);
-        if (isNaN(amount) || amount <= 0) {
-            alert("Жарамды сома енгізіңіз.");
-            continue;
-        }
-        
-        
-        let convertedAmountInEuro = 0;
-        
-        if (currency === "USD") {
-            convertedAmountInEuro = amount * 0.85; 
-        } else if (currency === "KZT") {
-            convertedAmountInEuro = amount * 0.002; 
-        } else if (currency === "EUR") {
-            convertedAmountInEuro = amount; 
+        let audaru = prompt("Валютаны енгізіңіз (USD, KZT, EUR):");
+        let valyuta1;
+
+        if (audaru === "USD") {
+            valyuta1 = 0.85;
+        } else if (audaru === "KZT") {
+           valyuta1 = 0.002;
+        } else if (audaru === "EUR") {
+            valyuta1 = 1;
         } else {
-            alert("Қате валютаның коды. USD, KZT немесе EUR таңдаңыз.");
+           alert("Қате: дұрыс валютаны таңдаңыз (USD, KZT, EUR)!");
             continue;
         }
+
         
-        
-        totalAmountInEuro += convertedAmountInEuro;
-        alert(`Бұл сома еуроға: ${convertedAmountInEuro.toFixed(2)} EUR`);
+        let kobeitu = summa * valyuta1;
+        euro += kobeitu;
+
+        alert(`Енгізілген сома: ${summa} ${audaru}. Евроға аударылды: ${kobeitu} EUR.`);
     }
-    
-   
-    alert(`Жалпы еуро сомасы: ${totalAmountInEuro.toFixed(2)} EUR`);
+alert(`Барлық соманың евродағы қосындысы: ${euro} EUR.`);
 }
 
 
-convertToEuro();
+convert();
