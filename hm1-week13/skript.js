2c3add388d8627328194ae44c74b04ab35e2cc5c
Miras <miras.ismail.2003@mail.ru>
2024-11-26 21:48:01 +0600

    homeWork

---
+++ hm1-week13/skript.js
@@ -0,0 +1,14 @@
+let pleyerPerson = ["Раналду","Зидан","Неймар","Бензема"];
+console.log(pleyerPerson);
+pleyerPerson[1] = "месси"
+pleyerPerson[4] = "Холанд"
+pleyerPerson[5] = "Какка"
+console.log(pleyerPerson);
+if (person.lenth>5) {
+    console.log(коп);
+    
+}else{
+    console.log(аз);
+    
+}
+
