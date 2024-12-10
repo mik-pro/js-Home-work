6728309faae6b4d20a7e3382753993c333fca70d
Miras <miras.ismail.2003@mail.ru>
2024-12-10 21:14:18 +0600

    homeWork-Introduction to DOM

---
+++ hm1-week15/script.js
@@ -0,0 +1,11 @@
+let taqyrip = document.querySelector('#new-taqyrip')
+taqyrip.textContent="HW: Introduction to DOM"
+
+let name = document.getElementById('new-name')
+name.textContent="Ismail Miras"
+
+let hobby = document.getElementsByClassName('new-hobby')
+hobby[0].innerHTML="<b>Football</b>"
+hobby[1].innerHTML="<b>Travel</b>"
+hobby[2].innerHTML="<b>Wotch move</b>"
+
