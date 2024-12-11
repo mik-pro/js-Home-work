9646e7fc13869109e72bc54e24998d39188839b8
Miras <miras.ismail.2003@mail.ru>
2024-12-11 23:28:16 +0600

    hmwork

---
+++ hm2-week15/script.js
@@ -0,0 +1,19 @@
+function buttonClick() {
+    let qoraptar=document.querySelector('#qorap')
+    qoraptar.style.backgroundColor="green"
+    
+}
+function buttonClick1() {
+    let batirma=document.querySelector('#qorap')
+    batirma.style.width="220px"
+   
+}
+function buttonClick2() {
+    let batirma=document.querySelector('#qorap')
+    batirma.style.height="220px"
+   
+}
+let qoraps=document.querySelector('#qorap')
+qoraps.addEventListener("mouseenter",function () {
+    qoraps.style.hover="jrhure"
+})
